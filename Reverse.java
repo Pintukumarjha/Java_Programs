d4a215a2b00ecde4f986da0bb31eb6debfb510e8
Pintu Kumar Jha <kumar.pintujha76@gmail.com>
2026-01-10 21:52:04 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+class Reverse
+{
+    public static void main(String args[])
+    {
+       String s="Apple";
+       String rev="";
+       
+       for(int i=s.length()-1;i>=0;i--)
+       {
+           rev=rev+s.charAt(i);
+       }
+       
+       System.out.println(rev);
+    }
+}
