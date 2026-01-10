7c2a8df9fcb4b0d3d2ac5daaffedc79c88bd81b8
Pintu Kumar Jha <kumar.pintujha76@gmail.com>
2026-01-10 21:46:15 +0530

    Update Ascending.java

--- Ascending.java
+++ Ascending.java
@@ -1,1 +1,26 @@
-
+class Main
+{
+    public static void main(String args[])
+    {
+       int[] a={1,4,2,7};
+       int temp=0;
+       
+       for(int i=0; i<a.length;i++)
+       {
+           for(int j=i+1; j<a.length;j++)
+           {
+               if(a[i]>a[j])
+               {
+                   temp=a[i];
+                   a[i]=a[j];
+                   a[j]=temp;
+               }
+           }
+       }
+       
+       for(int i=0;i<a.length;i++)
+       {
+           System.out.println(a[i]);
+       }
+    }
+}
