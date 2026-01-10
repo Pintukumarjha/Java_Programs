56c79094ba1584dab56c62f7d378259ab999388a
Pintu Kumar Jha <kumar.pintujha76@gmail.com>
2026-01-10 21:40:21 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String args[])
+    {
+       int a=5;
+       int fact=1;
+       
+       for (int i=1;i<=5;i++)
+       {
+           fact=fact*i;
+       }
+       
+       System.out.println(fact);
+    }
+}
