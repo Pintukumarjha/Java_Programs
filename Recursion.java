fb0d95f10863ba8a61de011f21dd87529022f7fc
Pintu Kumar Jha <kumar.pintujha76@gmail.com>
2026-01-11 19:36:36 +0530

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,22 @@
+class Recursion
+{
+     
+       static int fact(int n)
+        {
+            if(n==0 || n==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*fact(n-1);
+            }
+        }
+    
+    
+    public static void main(String args[])
+    {
+       int x=fact(5);
+       System.out.println("Factorial of a number is:"+x);
+    }
+}
